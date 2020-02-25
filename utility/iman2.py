a54a7bd0b02ad9fa105faaf8ef888a26efecbe28
Ubuntu <cyyam@cyyamvm.oufrghqsmnhelgr12r2ferai5h.ax.internal.cloudapp.net>
2020-02-25 11:14:45 +0000

    Add resize_image into iman2

--- utility/iman2.py
+++ utility/iman2.py
@@ -1,3 +1,36 @@
+def resize_image(image, width=None, height=None, inter=None):
+    import cv2
+    
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    img_resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return img_resized
+
 def detect_rectangles2(img, contours, w_min, w_max, h_min, h_max):
     import numpy as np
     import cv2
